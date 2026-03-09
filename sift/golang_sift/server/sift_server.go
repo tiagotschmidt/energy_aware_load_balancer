676189f3f0b54eff8552b768d7352f819266da55
Tiago Schmidt <68293535+tiagotschmidt@users.noreply.github.com>
2026-03-09 19:38:51 -0300

    Refactor SIFT server: reduce MAX_VECTORS, fix throughput log filename, and improve error handling in request processing

--- sift/golang_sift/server/sift_server.go
+++ sift/golang_sift/server/sift_server.go
@@ -15,7 +15,7 @@ import (
 
 const LOG_DIR = "logs"
 const DATA_FILE = "sift_data/dataset.npy"
-const MAX_VECTORS = 1000000
+const MAX_VECTORS = 100000
 
 var (
 	port = kingpin.Flag("port", "Port to listen on").Default("8080").String()
@@ -53,7 +53,7 @@ func vectorSearch(query []float32, database []float32) int {
 }
 
 func monitorThroughput(identity string, intervalMS int) {
-	var filename = fmt.Sprintf("%s/%sthroughput.log", LOG_DIR, identity)
+	var filename = fmt.Sprintf("%s/%s_throughput.txt", LOG_DIR, identity)
 
 	fmt.Printf("--- Monitor started. Writing throughput to %s ---\n", filename)
 
@@ -65,15 +65,12 @@ func monitorThroughput(identity string, intervalMS int) {
 
 		tempFilename := fmt.Sprintf("%s.tmp", filename)
 
-		file, err := os.Open(tempFilename)
-		defer file.Close()
-		if err != nil {
-			fmt.Printf("Error: Could not open log file %s:%s\n", tempFilename, err)
-			return
-		}
-
 		writeString := fmt.Sprintf("%.2f", currentThroughput)
-		file.Write([]byte(writeString))
+		err := os.WriteFile(tempFilename, []byte(writeString), 0644)
+		if err != nil {
+			fmt.Printf("Error: Could not write to temp file %s: %v\n", tempFilename, err)
+			continue
+		}
 
 		os.Rename(tempFilename, filename)
 	}
@@ -83,7 +80,8 @@ func parseQueryVector(data []byte) []float32 {
 	returnVector := make([]float32, 128)
 
 	for i := 0; i < 128; i++ {
-		var currentValue = data[i : i+4]
+		start := i * 4
+		var currentValue = data[start : start+4]
 		var currentBits = binary.LittleEndian.Uint32(currentValue)
 		var currentFloat = math.Float32frombits(currentBits)
 
@@ -102,21 +100,26 @@ func handle_request(databaseVectors []float32, socket *net.UDPConn, address *net
 	}
 
 	if len(messageData) < 512 {
-		queryVector := parseQueryVector(messageData[:512]) // TODO: implement this function
-
-		requestID := string(messageData[515:])
-
-		result := vectorSearch(queryVector, databaseVectors) // TODO: implement this function
-		reply := fmt.Sprintf("Reply from %s ID:%s : Match %d", identity, requestID, result)
-
-		socket.WriteToUDP([]byte(reply), address)
-
-		durationMs := time.Since(startProcessing).Milliseconds()
-		csvFile.WriteString(fmt.Sprintf("%s,%s,%d,", startProcessing.Format(time.RFC3339), address.String(), durationMs))
-
-		// TODO INCREMENT requestCounbter
-		atomic.AddUint64(&globalRequestCounter, 1)
+		fmt.Printf("Received message too short from %s: %d bytes\n", address, len(messageData))
+		return
 	}
+
+	queryVector := parseQueryVector(messageData[:512]) // TODO: implement this function
+
+	requestID := string(messageData[515:])
+
+	result := vectorSearch(queryVector, databaseVectors) // TODO: implement this function
+	// fmt.Printf("Result: %d\n", result)
+
+	reply := fmt.Sprintf("Reply from %s ID:%s : Match %d", identity, requestID, result)
+
+	socket.WriteToUDP([]byte(reply), address)
+
+	durationMs := time.Since(startProcessing).Milliseconds()
+	csvFile.WriteString(fmt.Sprintf("%s,%s,%d,", startProcessing.Format(time.RFC3339), address.String(), durationMs))
+
+	// TODO INCREMENT requestCounbter
+	atomic.AddUint64(&globalRequestCounter, 1)
 }
 
 func loadDatabase(filename string) ([]float32, error) {
@@ -148,15 +151,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	socket, _ := net.ListenUDP(*port, &net.UDPAddr{})
+	fmt.Printf("--- Loaded SIFT data (%d vectors)---\n", MAX_VECTORS)
+
+	addr, err := net.ResolveUDPAddr("udp", ":"+*port)
+	if err != nil {
+		fmt.Printf("Error resolving address: %v\n", err)
+		os.Exit(1)
+	}
+
+	socket, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Printf("Error starting UDP listener: %v\n", err)
+		fmt.Println("TIP: Check if another server is already running on this port.")
+		os.Exit(1)
+	}
+	defer socket.Close()
 
 	csvFile, _ := os.Create(fmt.Sprintf("%s/%s_work.csv", LOG_DIR, *id))
 
 	go monitorThroughput(*id, 1000)
 
+	fmt.Printf("--- Server Listening on UDP %s ---\n", *port)
+
 	for true {
 		data := make([]byte, 2048)
-		_, address, _ := socket.ReadFromUDP(data)
-		go handle_request(database, socket, address, *id, csvFile, data)
+		n, address, err := socket.ReadFromUDP(data)
+
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			continue
+		}
+
+		go handle_request(database, socket, address, *id, csvFile, data[:n])
 	}
 }
